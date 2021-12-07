3de43fc837166ef326ed0c3ac713568de87d3027
jake-ephraim <ephraimakolo2017@gmail.com>
2021-12-07 19:10:57 +0100

    bug fixes and update

--- src/main.py
+++ src/main.py
@@ -5,34 +5,55 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import OneLineListItem
+from kivymd.uix.list import OneLineListItem, OneLineAvatarIconListItem, TwoLineAvatarIconListItem, IRightBodyTouch
 from kivy.lang import Builder
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import FadeTransition, ScreenManager, NoTransition
 from kivy.properties import ObjectProperty
+from kivy.metrics import dp, sp
 from kivy.clock import Clock
 from directories import *
-#REMOVE IN PRODUCTION
+#REMOVE/MODIFY IN PRODUCTION
 # from kivy.core.window import Window
-from kivy.metrics import dp, sp
 # Window.size = (dp(428/1.5), dp(926/1.5))
 # from decrypt import decryptFile
 # decryptFile("")
 ##############################
 
+
+class StarListItem(IRightBodyTouch, MDBoxLayout):
+    adaptive_width = True
+
+
+class OneItemList(OneLineAvatarIconListItem):
+    pass
+
+
+class TwoItemList(TwoLineAvatarIconListItem):
+    pass
+
+
 class MenuListItem(OneLineListItem):
     # This is an extension of the ListItem used in the Dropdown menu.
     # It tries to align the labels to the center which was previously more difficult
+    # used in the app's menu
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.ids._lbl_primary.halign = "center"
 
 
 class BottomBar(RelativeLayout):
-    # Bottom Navigation bar of the app.
-    # This Class tries to modify the capitalization and color issues encountered.
+    # Bottom Navigation bar of the app, it includes the recent, local, cloud, and starred pages.
+    # This clase extends the pages/tabs functionality.
+    # This Class can modify the capitalization and color issues encountered.
     # This class converts the upper case tab labels to captalized labels.
     # And changes the original blue tab color to our defined color.
+
+    #REMOVE/MODIFY IN PRODUCTION
+    #for testing purposes
+    local_files = [['Project description', 'Today', i%2] if i < 15 else ['Project description', 'Yesterday', 0] for i in range(30)]
+    ################################
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.bind(on_kv_post = self.run_once)
@@ -43,6 +64,7 @@ class BottomBar(RelativeLayout):
         It also tries to access the tab object to change the font  and binds the color to a function, 
         calling it when there is a change in the color value.
         '''
+        self.assign_file_to_localTab()
         for i in range(len(self.children[0].children[0].children[0].children)):
             self.children[0].children[0].children[0].children[i].children[0].children[0].font_style = "Subtitle2"
             self.children[0].children[0].children[0].children[i]._text_color_normal = (80/255, 52/255, 24/255, 1) if i == len(self.children[0].children[0].children[0].children)-1 else (80/255, 52/255, 24/255, 0.38)
@@ -54,8 +76,20 @@ class BottomBar(RelativeLayout):
         '''
         inst._text_color_normal = (80/255, 52/255, 24/255, rgba[-1])
 
+    def assign_file_to_localTab(self):
+        #get the list items
+        file_list = self.local_files
+        for name, day, favourite in file_list:
+            self.ids.recent_list.add_widget(TwoItemList(text=name, secondary_text=day))
+            self.ids.library_list.add_widget(OneItemList(text=name))
+            self.ids.cloud_list.add_widget(OneItemList(text=name))
+            self.ids.starred_list.add_widget(OneItemList(text=name))
 
-class RecentWindow(MDScreen):
+        
+
+
+
+class TabsWindow(MDScreen):
     pass
 
 
@@ -67,15 +101,18 @@ class LoginWindow(MDScreen):
         
 
 class MainWidget(ScreenManager):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.transition = NoTransition()
-        self.bind(on_kv_post = self.run_once)
+        self.bind(on_kv_post = self._run_once)
     
-    def run_once(self, d, t):
-        self.current = "signup"
+    def _run_once(self, d, t):
         self.current = "login"
-        pass
+        Clock.schedule_once(self._open_first_screen)
+
+    def _open_first_screen(self, d):
+        self.current = 'tabs'
 
     def focus_login(self):
         '''
@@ -113,7 +150,7 @@ class TMSApp(MDApp):
 
     def build(self):
         # Menu Definition
-        menu_names = ["About", "Help", "Profile", "Settings", "Sign out"]
+        menu_names = ["About", "Settings", "Sign in", "Sign out"]
         menu_items = [
             {
                 "viewclass": "MenuListItem",
@@ -126,7 +163,7 @@ class TMSApp(MDApp):
         Builder.load_file(uiSettings_file)
         Builder.load_file(loginWindow_file)
         Builder.load_file(signupWindow_file)
-        Builder.load_file(recent_file)
+        Builder.load_file(tabs_file)
         Builder.load_file(components_file)
         rootWindow = Builder.load_file(mainKivy_file)
         return rootWindow
